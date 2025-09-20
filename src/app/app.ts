662000994eac0baa2ee31d845a3e90699e236cce
@Chriford⚡Siame <chrifordsiame@gmail.com>
2025-09-20 23:24:29 +0200

    implement memory listing page

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import {NzImageModule} from 'ng-zorro-antd/image'
 
 @Component({
   selector: 'app-root',
-  imports: [NzIconModule, NzLayoutModule, NzMenuModule],
+  imports: [NzIconModule, NzLayoutModule, NzMenuModule, NzImageModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
