57f50eb906162f6f3078acda7fc8cdd18fa89f9e
Siame <chrifordsiame@gmail.com>
2025-09-20 20:57:41 +0100

    update antd configs

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'radical-gradient': 'radial-gradient(circle, #F200FF4D, #EBFBFF)',
+      },
+    },
+  },
+  plugins: [],
+}
