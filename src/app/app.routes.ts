841974ec6a613f69d093ec49f6c01f4156a23a26
chriftech <chrifordsiame@gmail.com>
2025-12-21 23:58:38 +0200

    Sidebar modification

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { MemoryListing } from './components/memory/memory-listing';
 import { Settings } from './components/memory/settings';
+import { PricingComponent } from './components/pricing/pricing.component';
 
 export const routes: Routes = [
   // { path: '', pathMatch: 'full', redirectTo: '', component: App },
   { path: '', component:  MemoryListing},
-  { path: 'settings/', component:  Settings},
+  { path: 'pricing', component:  PricingComponent},
+  { path: 'settings', component:  Settings}
 ];
