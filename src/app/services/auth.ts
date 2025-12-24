b06fbee59951ffe7fd0efa0f40bab5d5cdadeebd
chriftech <chrifordsiame@gmail.com>
2025-12-24 17:24:23 +0200

    Auth guard implementation

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@angular/core';
+import {
+  Auth,
+  browserSessionPersistence,
+  GoogleAuthProvider,
+  setPersistence,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  user,
+  User,
+} from '@angular/fire/auth';
+import { from, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class Auth {
-  
+
+export class AuthService {
+
+  user$: Observable<any | null>;
+  constructor(private firebaseAuth: Auth) {
+    this.setSessionStoragePersistence();
+    this.user$ = user(this.firebaseAuth);
+  }
+
+  private setSessionStoragePersistence(): void {
+    setPersistence(this.firebaseAuth, browserSessionPersistence);
+  }
+
+  login(email: string, password: string): Observable<void> {
+    const promise = signInWithEmailAndPassword(
+      this.firebaseAuth,
+      email,
+      password
+    ).then((response) => {
+      console.log("LOGIN RESPONSE: ", response)
+    });
+    return from(promise);
+  }
+
 }
