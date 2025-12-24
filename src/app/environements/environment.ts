0a07b37edcdc2759bda999f250f1baa1b570d5c1
chriftech <chrifordsiame@gmail.com>
2025-12-24 17:23:50 +0200

    Firebase auth configuration

---
+++ src/app/environements/environment.ts
@@ -0,0 +1,16 @@
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCDR5fJU8y_nqYJMRdLZ6MUIE0cuRXG8Jc",
+    authDomain: "gallary-c99ab.firebaseapp.com",
+    projectId: "gallary-c99ab",
+    storageBucket: "gallary-c99ab.firebasestorage.app",
+    messagingSenderId: "51829541372",
+    appId: "1:51829541372:web:cf79857d4a972fef4fca79",
+    measurementId: "G-C0C646HF32"
+  }
+};
